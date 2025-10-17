8dde08028465812ac2ba6c1c66f2d3dd55d112b0
Nexterian <daniela.preto17@gmail.com>
2025-10-17 09:38:21 -0300

    agregar casa relicaria

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -50,8 +50,20 @@ const Navigation = () => {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">
-          <h1>Relicaria</h1>
+        <Link to="/" className="logo-link">
+          <h1 className="logo-text">Relicaria</h1>
+          <svg 
+            version="1.0" 
+            xmlns="http://www.w3.org/2000/svg" 
+            className="logo-icon" 
+            viewBox="0 0 64 64"
+          >
+            <path 
+              d="M62.79,29.172l-28-28C34.009,0.391,32.985,0,31.962,0s-2.047,0.391-2.828,1.172l-28,28
+              c-1.562,1.566-1.484,4.016,0.078,5.578c1.566,1.57,3.855,1.801,5.422,0.234L8,33.617V60c0,2.211,1.789,4,4,4h16V48h8v16h16
+              c2.211,0,4-1.789,4-4V33.695l1.195,1.195c1.562,1.562,3.949,1.422,5.516-0.141C64.274,33.188,64.356,30.734,62.79,29.172z"
+            />
+          </svg>
         </Link>
       </div>
 
