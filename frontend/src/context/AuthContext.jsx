efaced72c491ebd525fbc304b060c29a7f62f12d
Val F <valentinferreira05@gmail.com>
2025-10-29 08:18:06 -0300

    lal

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -14,7 +14,7 @@ export const AuthProvider = ({ children }) => {
       if (!token) return;
 
       try {
-        const response = await fetch("http://localhost:8080/users/me", {
+        const response = await fetch("http://localhost:8080/users/", {
           headers: { Authorization: `Bearer ${token}` },
         });
         if (response.ok) {
