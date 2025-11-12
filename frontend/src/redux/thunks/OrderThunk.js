3ec63fe98a7881e7c178ebac9af71ef423b0c557
Agostina Bernard <agostinabernard@MacBook-Air-de-Agostina-2.local>
2025-11-12 09:13:40 -0300

    axios in OrderThunk

--- frontend/src/redux/thunks/OrderThunk.js
+++ frontend/src/redux/thunks/OrderThunk.js
@@ -1,4 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const URL = "http://localhost:8080";
 
 export const checkoutOrder = createAsyncThunk(
   "order/checkoutOrder",
@@ -6,23 +9,17 @@ export const checkoutOrder = createAsyncThunk(
     const token = localStorage.getItem("jwtToken");
     if (!token) return rejectWithValue("No token found");
 
-    try {
-      const response = await fetch("http://localhost:8080/cart/checkout", {
-        method: "POST",
+    const { data } = await axios.post(
+      `${URL}/cart/checkout`,
+      {},
+      {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
-      });
-      if (!response.ok) {
-        const message = await response.text();
-        return rejectWithValue(message || "Checkout failed");
       }
+    );
 
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
+    return data;
   }
 );
